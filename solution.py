cc38a2fc64caf7ae38caedc19d8e58919493627d
xatriya <akash.khamkar40@gmail.com>
2019-11-08 18:29:59 +0530

    google foobar answers for my trip

---
+++ solution.py
@@ -0,0 +1,9 @@
+letters=[chr(ord('a')+i) for i in range(26)]
+def solution(x):
+	result=""
+	for i in range(len(x)-1):
+		if x[i] in letters:
+			result+=letters[25-i]
+		else:
+			result+=x[i]
+	return result
