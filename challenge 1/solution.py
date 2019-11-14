897fedc9e4831d87f122b7c91cad79fbd16d1cb6
xatriya <akash.khamkar40@gmail.com>
2019-11-14 20:30:58 +0530

    fix

--- challenge 1/solution.py
+++ challenge 1/solution.py
@@ -3,7 +3,7 @@ def solution(x):
 	result=""
 	for i in range(len(x)-1):
 		if x[i] in letters:
-			result+=letters[25-i]
+			result+=letters[25-letters.index(x[i])]
 		else:
 			result+=x[i]
 	return result
